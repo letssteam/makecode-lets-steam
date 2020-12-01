0867d924afea434bad97f4501fdea577ef4128e5
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2020-12-01 16:38:26 +0200

    Update action to clone submodule

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 scripts/install-pxt.sh
-scripts/install-pxt-common-package.sh
+scripts/install-pxt-common-packages.sh
 scripts/install-pxt-lets-steam.sh
-scripts/install-pxt-cli.sh
+#scripts/install-pxt-cli.sh
 scripts/link.sh
