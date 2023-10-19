9a75ea2fb18c68a98ddfa754c24367a7cc72079f
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2023-10-19 16:01:52 +0000

    script now make a deep clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,14 @@ clean() {
     cd pxt-lets-steam || exit
     pxt clean
     cd ..
+    rm -Rf pxt/node_modules
+    rm -Rf pxt-common-packages/node_modules
+    rm -Rf pxt-lets-steam/node_modules
+    
+    rm -f package-lock.json
+    rm -f pxt/package-lock.json
+    rm -f pxt-common-packages/package-lock.json
+    rm -f pxt-lets-steam/package-lock.json
 }
 
 clean
