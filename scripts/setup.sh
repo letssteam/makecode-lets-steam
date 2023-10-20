d8dbf1b8cd86c849959f312bf59baa811c758f77
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2023-10-20 12:57:47 +0000

    disable link

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 scripts/install-pxt.sh
-scripts/install-pxt-common-packages.sh
-scripts/install-pxt-lets-steam.sh
+#scripts/install-pxt-common-packages.sh
+#scripts/install-pxt-lets-steam.sh
 scripts/install-pxt-cli.sh
-scripts/link.sh
+#scripts/link.sh
 scripts/build.sh
